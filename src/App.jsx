54f4886bb3c34aded22e7b09efb841f8354deacd
Ankit <ankit.dayal1992@gmail.com>
2025-04-13 01:36:26 +0530

    gameWon condition added and Roll Btn CSS Updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { nanoid } from "nanoid";
 export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
 
+  const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
+
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
@@ -48,7 +50,7 @@ export default function App() {
       </p>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
-        Roll
+        {gameWon ? 'New Game' : 'Roll'}
       </button>
     </main>
   );
