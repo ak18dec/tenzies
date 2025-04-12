dab46f61b281775e6431f0774908ab51576ee4e0
Ankit <ankit.dayal1992@gmail.com>
2025-04-13 01:56:06 +0530

    New Game functionality added, lazy state initialisation added for generating new numbers

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { nanoid } from "nanoid";
 import Confetti from 'react-confetti'
 
 export default function App() {
-  const [dice, setDice] = useState(generateAllNewDice());
+  const [dice, setDice] = useState(() => generateAllNewDice());
 
   const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
 
@@ -27,12 +27,16 @@ export default function App() {
   ));
 
   function rollDice() {
-    setDice(oldDice => oldDice.map(die => 
-      die.isHeld ? 
-        die : 
-        {...die, value: Math.ceil(Math.random() * 6)}
+    if(!gameWon) {
+      setDice(oldDice => oldDice.map(die => 
+        die.isHeld ? 
+          die : 
+          {...die, value: Math.ceil(Math.random() * 6)}
+        )
       )
-    )
+    }else {
+      setDice(generateAllNewDice())
+    }
   }
 
   function hold(id) {
