316a68e7795546435d36ba068eb74f0b9ab3badd
Ankit <ankit.dayal1992@gmail.com>
2025-04-13 02:10:51 +0530

    Accessibility Updates - Adding focus to new game btn on game won

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import Die from "./Die";
 import { nanoid } from "nanoid";
@@ -6,8 +6,15 @@ import Confetti from 'react-confetti'
 
 export default function App() {
   const [dice, setDice] = useState(() => generateAllNewDice());
+  const buttonRef = useRef(null)
 
   const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
+
+  useEffect(() => {
+    if(gameWon) {
+      buttonRef.current.focus()
+    }
+  }, [gameWon])
 
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
@@ -65,7 +72,7 @@ export default function App() {
         Click each die to freeze it at its current value between rolls.
       </p>
       <div className="dice-container">{diceElements}</div>
-      <button className="roll-dice" onClick={rollDice}>
+      <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
         {gameWon ? 'New Game' : 'Roll'}
       </button>
     </main>
